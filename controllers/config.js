d1cf8eaca898974e4bbb19c738eeb47e8b6ec28b
Ehsan200 <ehsan.movaffagh@icloud.com>
2023-02-19 15:10:18 +0330

    feat: Add docker postgres location

--- controllers/config.js
+++ controllers/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     user: 'postgres', //your pg user
-    host: 'localhost',
-    database: 'hw2',  //your db
+    host: 'ticket-postgres-0',
+    database: 'postgres',  //your db
     password: 'postgres', //your pg user password
     port: 5432, //default port
 }
